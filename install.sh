b707442fe42f6ee51a8645b4966179b82367d758
alateas <dmitrymashkin@gmail.com>
2012-10-30 16:41:13 +0400

    ololo

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo apt-get install python-pip
 sudo pip install virtualenvwrapper
 sudo mkdir /home/djangoprojects
 echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
-echo 'export PROJECT_HOME=/home/djangoprojects' >> ~/.bashrc
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
-
+echo 'export PIP_VIRTUALENV_BASE=$WORKON_HOME' >> ~/.bashrc
+echo 'export PIP_RESPECT_VIRTUALENV=true' >> ~/.bashrc
 
 export DATABASE_URL="mysql://outlets_dev:outlets_dev@192.168.106.10/outlets_dev"
