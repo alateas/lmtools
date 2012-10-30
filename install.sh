66a75a8ccb5c1711048c98c8ade18a142f0415dc
alateas <dmitrymashkin@gmail.com>
2012-10-30 13:38:57 +0400

    added DATABASE_URL

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
+
+export DATABASE_URL="mysql://outlets_dev:outlets_dev@192.168.106.10/outlets_dev"
