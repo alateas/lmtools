aae0bce892b216069dc495e8170c114c911c0784
alateas <dmitrymashkin@gmail.com>
2012-10-30 15:12:54 +0400

    added virualenvwrapper

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
 
+sudo apt-get install pip
+sudo pip install virtualenvwrapper
+sudo mkdir /home/djangoprojects
+echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
+echo 'export PROJECT_HOME=/home/djangoprojects' >> ~/.bashrc
+echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+
+
 export DATABASE_URL="mysql://outlets_dev:outlets_dev@192.168.106.10/outlets_dev"
