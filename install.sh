64b913e44687a4b2b74b88a2ecb5bdfbb950cab8
alateas <dmitrymashkin@gmail.com>
2012-10-31 14:53:59 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+BASEDIR=$(dirname $0)
+
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
 
 apt-get -y update
 apt-get -y install libmysqlclient-dev build-dep python-mysqldb
-
 
 sudo apt-get install python-pip
 sudo pip install virtualenvwrapper
@@ -13,3 +14,7 @@ echo 'export PIP_VIRTUALENV_BASE=$WORKON_HOME' >> ~/.bashrc
 echo 'export PIP_RESPECT_VIRTUALENV=true' >> ~/.bashrc
 
 export DATABASE_URL="mysql://outlets_dev:outlets_dev@192.168.106.10/outlets_dev"
+
+cd $BASEDIR
+mkvirtualenv mkvirtualenv
+pip install -r requirements.txt
