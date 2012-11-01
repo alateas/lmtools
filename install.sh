1746fa2863769c18ca3a0a978516917d74353721
alateas <dmitrymashkin@gmail.com>
2012-11-01 15:47:23 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,9 +18,9 @@ sudo apt-get -y install python-pip
 #install and setup virtualenvwrapper http://pypi.python.org/pypi/virtualenvwrapper
 sudo pip install virtualenvwrapper
 echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
-echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 echo 'export PIP_VIRTUALENV_BASE=$WORKON_HOME' >> ~/.bashrc
 echo 'export PIP_RESPECT_VIRTUALENV=true' >> ~/.bashrc
+echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 
 #set default database url
 export DATABASE_URL="mysql://outlets_dev:outlets_dev@192.168.106.10/outlets_dev"
@@ -30,10 +30,15 @@ cd ~
 pwd > /home/user/pwd.txt
 cat ~/.bashrc > /home/user/bashrc.txt
 
-#create new virualenv
-source ~/.bashrc > /home/user/bashrc.log
-cd $SCRIPTPATH
-mkvirtualenv outlets > /home/user/mkvirtualenv.log
+#use virtualenvwrapper
+export WORKON_HOME='~/.virtualenvs'
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+export PIP_RESPECT_VIRTUALENV=true
+source /usr/local/bin/virtualenvwrapper.sh
+
+#create new virtualenv
+mkvirtualenv outlets
+workon outlets
 
 #install libs to new virtualenv
 pip install -r requirements.txt > /home/user/pipinstall.log
