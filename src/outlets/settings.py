ea6d76b8f7a5202c1f030a8810ded3507783074e
alateas <dmitrymashkin@gmail.com>
2012-11-01 17:08:42 +0400

    Update src/outlets/settings.py

--- src/outlets/settings.py
+++ src/outlets/settings.py
@@ -10,13 +10,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='mysql://...')}
+DATABASES = {'default': dj_database_url.config(default='mysql://outlets_dev:outlets_dev@192.168.106.10/outlets_dev')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
