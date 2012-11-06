c5dfedd138b9dc9b35d40e1417937f929541818b
alateas <dmitrymashkin@gmail.com>
2012-11-06 15:58:37 +0400

    added fabfile

---
+++ fabfile.py
@@ -0,0 +1,30 @@
+""" Deployment of your django project.
+"""
+
+from fabric.api import *
+
+env.hosts = ['web']
+env.user = "user"
+
+def update_django_project():
+    """ Updates the remote django project.
+    """
+    with cd('/home/djangoprojects/outlets'):
+        run('git pull')
+        with prefix('source /home/user/.virtualenvs/outlets/bin/activate'):
+            run('pip install -r requirements.txt')
+            run('python /src/manage.py syncdb')
+            run('python /src/manage.py migrate') # if you use south
+            run('python /src/manage.py collectstatic --noinput')
+
+def restart_webserver():
+    """ Restarts remote nginx and uwsgi.
+    """
+    sudo("service uwsgi restart")
+    sudo("/etc/init.d/nginx restart")
+
+def deploy():
+    """ Deploy Django Project.
+    """
+    update_django_project()
+    restart_webserver()
