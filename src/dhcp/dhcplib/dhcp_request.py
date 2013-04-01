3d666df248c1d6c895bf4394522106ed60e1b5d6
alateas <dmitrymashkin@gmail.com>
2013-04-01 15:43:45 +0400

    dhcp_request

---
+++ src/dhcp/dhcplib/dhcp_request.py
@@ -0,0 +1,19 @@
+import subprocess
+
+class DhcpRequest():
+    def __init__(self, server):
+        self.__server = server
+
+    def __server_call(self, command):
+        user = "alateas"
+        password = "ololo"
+        prefix = ["winexe", "-U", user, "--password=%s" % password, "//%s" % self.__server]
+        # result_cmd = "%s \"%s\"" % (prefix, command)
+        prefix.append(command)
+        print prefix
+        output = subprocess.check_output(prefix)
+        return output.split("\n")
+
+    def get_leases(self):
+        return self.__server_call("netsh dhcp server scope 192.168.104.0 show clients 0")
+        # return self.__server_call(["netsh", "dhcp", "server", "scope", "192.168.104.0", "show", "clients", "0"])        
