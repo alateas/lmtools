6947bd872e476642548eeff51ab727f91a63c0ed
alateas <dmitrymashkin@gmail.com>
2013-04-16 00:22:51 +0400

    added tornado simple page

---
+++ src/web/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os
+# import tornado
+# from basic import require_basic_auth
+# import ldapauth
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+# @require_basic_auth('Authrealm', ldapauth.auth_user_ldap)
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("base.html")
+
+def main():
+    static_path = '/home/alateas/lmtools/static'
+
+    tornado.options.parse_command_line()
+    application = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': static_path}),
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        debug=True
+    )
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
