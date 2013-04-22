3fbb85743aa2212602ae01df23b71136f4e928bc
alateas <dmitrymashkin@gmail.com>
2013-04-22 16:50:59 +0400

    refacored controller class, added support of getting all leases

--- src/daemons/dhcp_manager/controller.py
+++ src/daemons/dhcp_manager/controller.py
@@ -1,11 +1,37 @@
 from model import DhcpModel
 
+import leases_pb2
+import requests_pb2
+
+
 class DhcpController(object):
     def __init__(self):
         self.__model = DhcpModel()
 
     def request(self, body):
-        ip1, ip2 = self.__model.get_ip_range_from_pb(body)
+        ip1, ip2 = self.__get_ip_range_from_pb(body)
         print " [x] Request: %s %s" % (ip1, ip2,)
-        print "leases : %s" % (self.__model.get_pb_leases_by_range(ip1, ip2),)
-        return str(self.__model.get_pb_leases_by_range(ip1, ip2))
+        return self.__get_pb_leases_by_range(ip1, ip2)
+
+    def __get_ip_range_from_pb(self, pb_range_request):
+        pb_request = requests_pb2.IpRangeRequest()
+        pb_request.ParseFromString(pb_range_request)
+        return (pb_request.ip1, pb_request.ip2)
+
+    def __wrap_leases_to_pb(self, leases):
+        pb_leases = leases_pb2.LeasesSet()
+        for lease in leases:
+            pb_lease = pb_leases.lease.add()
+            pb_lease.name = lease.name
+            pb_lease.ip   = str(lease.ip)
+            pb_lease.mac  = str(lease.mac)
+
+        return pb_leases.SerializeToString()
+
+    def __get_pb_leases_by_range(self, ip1, ip2):
+        leases = self.__model.get_leases_by_range(ip1, ip2)
+        return self.__wrap_leases_to_pb(leases)
+
+    def __get_pb_all_leases(self):
+        leases = self.__model.get_all_leases()
+        return self.__wrap_leases_to_pb(leases)    
--- src/daemons/dhcp_manager/model.py
+++ src/daemons/dhcp_manager/model.py
@@ -1,7 +1,5 @@
 from dhcplib.dhcp import Dhcp
 from dhcplib.network import Ip
-import leases_pb2
-import requests_pb2
 from passw import info
 
 class DhcpModel(object):
@@ -11,19 +9,5 @@ class DhcpModel(object):
     def get_leases_by_range(self, ip1, ip2):
         return self.__dhcp.get_range(Ip.from_string(ip1), Ip.from_string(ip2))
 
-    def get_pb_leases_by_range(self, ip1, ip2):
-        leases = self.get_leases_by_range(ip1, ip2)
-        pb_leases = leases_pb2.LeasesSet()
-        for lease in leases:
-            pb_lease = pb_leases.lease.add()
-            
-            pb_lease.name = lease.name
-            pb_lease.ip   = str(lease.ip)
-            pb_lease.mac  = str(lease.mac)
-
-        return pb_leases.SerializeToString()
-
-    def get_ip_range_from_pb(self, pb_range_request):
-        pb_request = requests_pb2.IpRangeRequest()
-        pb_request.ParseFromString(pb_range_request)
-        return (pb_request.ip1, pb_request.ip2)
+    def get_all_leases(self):
+        return self.__dhcp.get_all()
