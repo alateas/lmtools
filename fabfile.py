60435f621ca9705631b798752444addf7453dad8
alateas <dmitrymashkin@gmail.com>
2013-06-28 14:07:31 +0400

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-""" Deployment of your django project.
+""" Deployment project.
 """
 
 from fabric.api import *
@@ -7,24 +7,35 @@ env.hosts = ['web']
 env.user = "user"
 
 def update_project():
-    """ Updates the remote django project.
+    """ Updates the remote project.
     """
     with cd('/opt/lmtools'):
-        run('git pull')
+        sudo('git pull')
         with prefix('source /home/user/.virtualenvs/lmtools/bin/activate'):
             run('pip install -r requirements.txt')
-            #run('python src/manage.py syncdb')
-            #run('python src/manage.py migrate') # if you use south
-            #run('python src/manage.py collectstatic --noinput')
+
+def restart_dhcp_daemon():
+    env.warn_only = True
+    with cd('/opt/lmtools/src/daemons/dhcp_manager'):
+        with prefix('source `which virtualenvwrapper.sh`'):
+            with prefix('workon lmtools'):
+                run('python main.py stop', pty=False)
+                run('python main.py start', pty=False)
+    env.warn_only = False
 
 def restart_webserver():
-    """ Restarts remote nginx and uwsgi.
-    """
-    sudo("service uwsgi_lmtools restart")
-    sudo("/etc/init.d/nginx restart")
+    env.warn_only = True
+    sudo("stop lmtools_web")
+    sudo("cp /opt/lmtools/lmtools_web.conf /etc/init")
+    sudo("start lmtools_web")
+    env.warn_only = False
+
+def restart_services():
+    restart_dhcp_daemon()
+    restart_webserver()
 
 def deploy():
-    """ Deploy Django Project.
+    """ Deploy project.
     """
-    update_django_project()
-    restart_webserver()
+    update_project()
+    restart_services()
