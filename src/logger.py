00e9d714f1dd9b515aec6a7df9b4bbc030a04a1c
alateas <dmitrymashkin@gmail.com>
2013-08-07 15:19:50 +0400

    new logging model

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,27 @@
 import logging
 import os.path as p
+import settings
 
 log_path = p.realpath(p.join(p.dirname(p.realpath(__file__)), '../logs/'))
+log_level = logging.DEBUG if settings.debug else logging.INFO
+
+# logging.config.fileConfig('logging.conf')
+
+def create_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    
+    formatter=logging.Formatter('[%(asctime)s]%(levelname)-8s|%(message)s')
+
+    handler=logging.FileHandler(p.join(log_path, name+'.log'))
+    handler.setFormatter(formatter)
+    handler.setLevel(log_level)
+
+    logger.addHandler(handler)
+    
+    return logger
+
+create_logger('dhcp_manager')
 
 def log(name, message, level = logging.INFO):
-    logging.basicConfig(format = u'[%(asctime)s]%(levelname)-8s|%(message)s', level = logging.INFO, filename=p.join(log_path, name+'.log'))
     logging.log(level, message)
